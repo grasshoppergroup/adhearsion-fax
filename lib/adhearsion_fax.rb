289614e94f4a8aeb50165eab2c352f6db9dcbb0d
Justin Aiken <60tonangel@gmail.com>
2013-11-04 10:44:02 -0700

    Initial commit

---
+++ lib/adhearsion_fax.rb
@@ -0,0 +1,4 @@
+AdhearsionFax = Module.new
+require "adhearsion_fax/version"
+require "adhearsion_fax/plugin"
+require "adhearsion_fax/controller_methods"
---
+++ lib/adhearsion_fax/version.rb
@@ -0,0 +1,3 @@
+module AdhearsionFax
+  VERSION = "0.0.1"
+end
